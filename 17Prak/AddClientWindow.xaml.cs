9b4e6b15f576c94550f3572a364895f5cd767a20
micro <micro@DESKTOP-FCFE4OD>
2025-02-27 20:14:33 +0300

    Добавьте файлы проекта.

---
+++ 17Prak/AddClientWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using MySql.Data.MySqlClient;
+using System.Windows;
+using System;
+
+namespace ClientApp
+{
+    public partial class AddClientWindow : Window
+    {
+        private string connectionString = "server=localhost;user=root;database=ClientDB;port=3306;password=;";
+
+        public AddClientWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO Clients (firstName, lastName, gender, address, city, phone, email, status, createdDate) VALUES (@firstName, @lastName, @gender, @address, @city, @phone, @email, @status, @createdDate)";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@firstName", FirstNameTextBox.Text);
+                command.Parameters.AddWithValue("@lastName", LastNameTextBox.Text);
+                command.Parameters.AddWithValue("@gender", GenderTextBox.Text);
+                command.Parameters.AddWithValue("@address", AddressTextBox.Text);
+                command.Parameters.AddWithValue("@city", CityTextBox.Text);
+                command.Parameters.AddWithValue("@phone", PhoneTextBox.Text);
+                command.Parameters.AddWithValue("@email", EmailTextBox.Text);
+                command.Parameters.AddWithValue("@status", StatusTextBox.Text);
+                command.Parameters.AddWithValue("@createdDate", DateTime.Now);
+                command.ExecuteNonQuery();
+            }
+
+            MessageBox.Show("Client added successfully!");
+            this.Close();
+        }
+    }
+}
---
+++ 17Prak/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace ClientApp
+{
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "server=localhost;user=root;database=ClientDB;port=3306;password=;";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadClients();
+        }
+
+        private void LoadClients()
+        {
+            List<Client> clients = new List<Client>();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Clients";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                MySqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    clients.Add(new Client
+                    {
+                        Id = reader.GetInt32("id"),
+                        FirstName = reader.GetString("firstName"),
+                        LastName = reader.GetString("lastName"),
+                        Gender = reader.GetString("gender"),
+                        Address = reader.GetString("address"),
+                        City = reader.GetString("city"),
+                        Phone = reader.GetString("phone"),
+                        Email = reader.GetString("email"),
+                        Status = reader.GetString("status"),
+                        CreatedDate = reader.GetDateTime("createdDate")
+                    });
+                }
+            }
+
+            ClientListView.ItemsSource = clients;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddClientWindow addClientWindow = new AddClientWindow();
+            addClientWindow.ShowDialog();
+            LoadClients();
+        }
+    }
+
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
